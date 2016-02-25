42a5d0415f121212a14f0c8f642353d0c80467f8
unknown <892280082>
2016-02-25 09:35:47 +0800

    schema

---
+++ models/ArticleComment.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    objectid = require('objectid');
+
+/**
+ * @desc 回复对象
+ */
+var replaySchema = new Schema({
+    _id:Schema.Types.ObjectId,//主键
+    _userId:Schema.Types.ObjectId,//回复用户外键
+    userName:String,//回复用户姓名
+    content:String,//回复内容
+    creatTime:{type:Date,default:Date.now},//回复时间
+    praiseCounts:Number,//赞次数
+})
+
+/**
+ * @desc 评论对象
+ */
+var commentSchema = new Schema({
+    _id:Schema.Types.ObjectId,//主键
+    _articleId:Schema.Types.ObjectId,//文章外键
+    _userId:Schema.Types.ObjectId,//评论用户外键
+    userName:String,//评论用户姓名
+    content:String,//评论内容
+    creatTime:{type:Date,default:Date.now},//评论时间
+    praiseCounts:Number,//赞次数
+    replays:[replaySchema]//回复数组
+})
+
--- models/article.js
+++ models/article.js
@@ -1,39 +1,45 @@
-/***
- * @desc  文章表
- * @date 2016/2/25
- * @auther yq
- */
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     objectid = require('objectid');
-
+/***
+ * @desc  文章对象
+ * @date 2016/2/25
+ * @auther yq
+ */
 var articleSchema = new Schema({
+    /**
+     * @desc 基本属性
+     */
     _id:Schema.Types.ObjectId,//主键
     _userId:Schema.Types.ObjectId,//用户Id
     title:String,//用户标题
-    checkcounts:Number,//查看次数
-    collectcounts:Number,//收藏次数
-    commentcounts:Number,//评论次数
-    imgUrl:String,//首页列表图
-    content:String,//文章内容
-    creatTime:{type:Date,default:Date.now},//创建时间
-    topno:Number,//展示在首页的顺序
-    from:String,//来源
-    isDelete:String,//是否被删除
     type:[],//文章类型
     introduce:String,//简介
+    creatTime:{type:Date,default:Date.now},//创建时间
+    authorName:String,//作者
+    keyword:[],//关键字
+    status:Number,// 作品 1-待审核 2-草稿
+    reason:String,//审核未通过原因
+    content:String,//文章内容
+    isDelete:String,//是否被删除
+    checkcounts:Number,//查看次数
+    collectcounts:Number,//收藏次数
+    /**
+     * @desc 特殊属性
+     * */
+    comments:[Schema.Types.ObjectId],//评论数组,储存评论_id
+    imgUrl:String,//首页列表图
+    topno:Number,//展示在首页的顺序
+    from:String,//来源
     otherno:[],//作者其他推荐文章
     relationno:[],//相关文章推荐
     articlefrom:[],//文章来源 0-名人 1-造物志
     bannerno:Number,//模块Banner展示
-    keyword:[],//关键字
-    authorName:String,//作者
     bigimgurl:String,//首页banner
     coverimgurl:[],//列表的图片
     bannerurl:[],//列表的banner
-    status:Number,// 作品 1-待审核 2-草稿
     sortno:Number,//排序
     attentionno:Number,//关注量
-    reason:String//审核未通过原因
 });
 
