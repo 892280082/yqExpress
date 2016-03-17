fe4f3330a8f7473840cdf475168dc743f9d8e92d
unknown <892280082>
2016-03-17 16:34:58 +0800

    ok

--- views/back/cateMannger/src/controller/main.js
+++ views/back/cateMannger/src/controller/main.js
@@ -25,9 +25,6 @@
                         $scope.webConfig.activeCates = $scope.webConfig.activeCates || [];
                         $scope.webConfig.productCates = $scope.webConfig.productCates || [];
                         $scope.webConfig.customerCates = $scope.webConfig.customerCates || [];
-
-                        console.log("data.result",$scope.webConfig);
-
                     }
                 }).error(function(data){
                     alert("获取错误");
@@ -41,6 +38,8 @@
                 subProductCate:'',
                 customerCate:''
             }
+
+            $scope.thirdProductCate={};
 
             $scope.tempEditArray = [];
 
@@ -87,6 +86,7 @@
 
             //删除一级分类
             var removeLowCate = function(catePojo,removePojo){
+                console.log("removeLowCate",catePojo,removePojo);
                 catePojo.remove(removePojo);
             }
 
@@ -98,11 +98,12 @@
             }
 
             //删除人物分类
-            $scope.removeCustomerCate = function(){
+            $scope.customerCate= {};
+            $scope.removeCustomerCate = function(removePojo){
                 var confirmFlag = $window.confirm("确定要删除吗,该操作不可恢复");
-                if(confirmFlag)
+                if(!confirmFlag)
                     return false;
-                removeLowCate($scope.webConfig.customerCates,$scope.customerCate);
+                removeLowCate($scope.webConfig.customerCates,removePojo);
             }
 
             //添加文章分类
@@ -112,11 +113,11 @@
             }
 
             //删除文章
-            $scope.removeArticleCate = function(){
+            $scope.removeArticleCate = function(removePojo){
                 var confirmFlag = $window.confirm("确定要删除吗,该操作不可恢复");
-                if(confirmFlag)
+                if(!confirmFlag)
                     return false;
-                removeLowCate($scope.webConfig.articleCates,$scope.articleCate);
+                $scope.webConfig.articleCates.remove(removePojo);
             }
 
             //添加活动分类
@@ -126,11 +127,11 @@
             }
 
             //删除活动分类
-            $scope.removeActiveCate = function(){
+            $scope.removeActiveCate = function(removePojo){
                 var confirmFlag = $window.confirm("确定要删除吗,该操作不可恢复");
-                if(confirmFlag)
+                if(!confirmFlag)
                     return false;
-                removeLowCate($scope.webConfig.activeCates,$scope.activeCate);
+                $scope.webConfig.activeCates.remove(removePojo);
             }
 
             //添加产品二级分类
@@ -141,24 +142,27 @@
 
 
             //删除产品二级分类
-            $scope.removeThirdProductCate = function(){
+            $scope.removeThirdProductCate = function(removePojo){
                 var confirmFlag = $window.confirm("确定要删除吗,该操作不可恢复");
-                if(confirmFlag)
+                if(!confirmFlag)
                     return false;
-                removeLowCate($scope.webConfig.productCates,$scope.thirdProductCate);
+                $scope.webConfig.productCates.remove(removePojo);
             }
 
 
             //添加产品二级子分类
-            $scope.addSubProductCate = function(){
-                addThirdCate($scope.thirdProductCate.subCate,$scope.tempPojo.subProductCate);
+            $scope.addSubProductCate = function(parent){
+                addThirdCate(parent.subCate,$scope.tempPojo.subProductCate);
                 $scope.tempPojo.subProductCate = "";
             }
 
 
             //删除产品二级子分类
-            $scope.removeSubProductCate = function(){
-                removeLowCate($scope.thirdProductCate.subCate,$scope.productSubCate);
+            $scope.removeSubProductCate = function(parent,child){
+                var confirmFlag = $window.confirm("确定要删除吗,该操作不可恢复");
+                if(!confirmFlag)
+                    return false;
+                removeLowCate(parent.subCate,child);
             }
 
             //深度更新
