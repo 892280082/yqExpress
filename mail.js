43ffc88f969d01af216116d146e4c9c41c4bc216
unknown <892280082>
2016-04-16 17:30:13 +0800

    OK

--- mail.js
+++ mail.js
@@ -1,18 +1,18 @@
-var TopClient = require('./util/ali_dayu/topClient').TopClient;
-var client = new TopClient({
-    'appkey': '23346796',
-    'appsecret': '12ca44482f2116044363aef32f27751a',
-    'REST_URL': 'http://gw.api.taobao.com/router/rest'
-});
- 
-client.execute('alibaba.aliqin.fc.sms.num.send', {
-    'extend':'123456',
-    'sms_type':'normal',
-    'sms_free_sign_name':'雅集文化传媒',
-    'sms_param':{code:"1234",product:"造物者"},
-    'rec_num':'18656139736',
-    'sms_template_code':'SMS_7480737'
-}, function(error, response) {
-    if (!error) console.log(response);
-    else console.log(error);
-});
+var then = require("thenjs");
+
+// then((next)=>{
+//     setTimeout(()=>{
+//         console.log("aa");
+//         next();
+//     },1000);
+// }).then((next)=>{
+//     console.log("bbb");
+//     next('出错了');
+// }).fail((next,err)=>{
+//     console.log(err);
+//     next();
+// }).then((next)=>{
+//     console.log("alreay deal err!");
+// });
+
+console.log(Buffer);
--- util/messageUtil.js
+++ util/messageUtil.js
@@ -1,18 +1,3 @@
-/**
- * @desc 封装大鱼发送验证码的API接口
- * @author yq
- * @date: 2016/4/16
- *
- * @API
- * 1.sendMsg            短信验证码发送接口
- * 2.sendRegistMsg      注册短信发送验证码
- * 3.sendChangePassword 更改密码发送短信验证码
- * 4.sendChangeSelfInfo 更改个人信息发送验证码
- * 5.sendJoinActiveMsg  参加活动发送验证码
- *
- */
-
-
 var TopClient = require('./ali_dayu/topClient').TopClient;
 var msgConfig = require('../conf/msg_config');
 
@@ -21,6 +6,7 @@ var client = new TopClient({
     'appsecret': msgConfig.appsecret,
     'REST_URL': 'http://gw.api.taobao.com/router/rest'
 });
+
 
 /**
  * @desc 基础短信发送方法 | 已经打印错误信息
@@ -101,52 +87,26 @@ exports.sendMsg = function(templateType,tel,random,param,callback){
     });
 };
 
-/**
- * @desc 注册短信发送该方法
- * @param tel {String|Number} -手机号
- * @param random {String|Number} -验证码
- * @param callback {Function} -回调函数
- */
+//注册短信发送该方法
 exports.sendRegistMsg = function(tel,random,callback){
     exports.sendMsg(1,tel,random,callback);
 };
 
-/**
- * @desc 更改密码发送短信方法
- * @param tel {String|Number} -手机号
- * @param random {String|Number} -验证码
- * @param callback {Function} -回调函数
- */
+//更改密码发送短信方法
 exports.sendChangePassword = function(tel,random,callback){
     exports.sendMsg(2,tel,random,callback);
 };
 
-/**
- * @desc 更改个人信息验证码
- * @param tel {String|Number} -手机号
- * @param random {String|Number} -验证码
- * @param callback {Function} -回调函数
- */
+//更改个人信息发送短信方法
 exports.sendChangeSelfInfo = function(tel,random,callback){
     exports.sendMsg(3,tel,random,callback);
 };
 
-/**
- * @desc 参加活动发送短信方法
- * @param activeName {String} -活动名称
- * @param tel {String|Number} -手机号
- * @param random {String|Number} -验证码
- * @param callback {Function} -回调函数
- */
+//15855512649 李贵军
+//15156822844 潘雅
+//18656139736 叶
+
 exports.sendJoinActiveMsg = function(activeName,tel,random,callback){
     exports.sendMsg(4,tel,random,{item:activeName},callback);
 };
-
-/**
- * @desc 测试短信人员手机号
- * 15855512649 李贵军
- * 15156822844 潘雅
- * 18656139736 叶
- * */
-
 
